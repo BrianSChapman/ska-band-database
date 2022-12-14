aec5ba6bcff6b08f28c20b0f4db171749f92fc39
Brian Chapman <chapman.brian86@gmail.com>
2022-12-14 16:53:18 -0500

    Initial setup, file creation, and installing dependencies

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'TikiVibes12!',
+    database: 'classlist_db'
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
