dd9e924014f7434b7fc259ce9b69c9e9d1a554da
Brian Chapman <chapman.brian86@gmail.com>
2022-12-20 17:25:36 -0500

    Reworking functions and updated ReadMe

--- index.js
+++ index.js
@@ -12,65 +12,66 @@ const {
 } = require("./queries");
 require("dotenv").config();
 
+const db = mysql.createConnection({
+  host: "localhost",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
 console.log(`Connected to the Ska Band database.`);
 startPrompt();
 
-async function startPrompt() {
-  try {
-    inquirer
-      .prompt({
-        type: "list",
-        message: "Welcome! What would you like to do?",
-        name: "welcome",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
-          "I'm done",
-        ],
-      })
-      .then(
-        await function (answer) {
-          console.log(answer);
-          switch (answer.welcome) {
-            case "View all departments":
-              viewDepartments();
-              break;
+function startPrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Welcome! What would you like to do?",
+      name: "welcome",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "I'm done",
+      ],
+    })
+    .then(function (answer) {
+      console.log(answer);
+      switch (answer.welcome) {
+        case "View all departments":
+          viewDepartments();
+          break;
 
-            case "View all roles":
-              viewRoles();
-              break;
+        case "View all roles":
+          viewRoles();
+          break;
 
-            case "View all employees":
-              viewEmployees();
-              break;
+        case "View all employees":
+          viewEmployees();
+          break;
 
-            case "Add a department":
-              addDepartment();
-              break;
+        case "Add a department":
+          addDepartment();
+          break;
 
-            case "Add a role":
-              addRole();
-              break;
+        case "Add a role":
+          addRole();
+          break;
 
-            case "Add an employee":
-              addEmployee();
-              break;
-            case "Update an employee role":
-              updateRole();
-              break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateRole();
+          break;
 
-            case "I'm done":
-              process.exit();
-          }
-        }
-      );
-  } catch (err) {
-    console.log(err);
-    startPrompt();
-  }
+        case "I'm done":
+          process.exit();
+      }
+    });
+    
 }
